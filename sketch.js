34dd5939f0e08e7d689d6581d27fdcd89fd22c1d
srijandubey23 <srijandubey639@gmail.com>
2021-04-22 17:10:50 +0530

    json and api

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+    getTime();
 }
 
 function setup(){
@@ -45,6 +46,7 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
@@ -85,4 +87,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getTime(){
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseType= await response.json();
+    console.log(responseType);
+    var dt = responseType.datetime;
+    console.log(dt);
+    var hours = dt.slice(11,13);
+    console.log(hours);
+    if(hours>=06  && hours<=19){
+        bg="sprites/bg.png"
+    }else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg);
 }
